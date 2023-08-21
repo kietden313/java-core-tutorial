6efc90892fdd23fe997d4369a8bd84d2fbaffed2
kietden313 <kietden3132001@gmail.com>
2023-08-21 15:55:06 +0700

    Update

--- nangcao3/src/Consumer.java
+++ nangcao3/src/Consumer.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Consumer {
+import java.util.Queue;
+
+public class Consumer implements Runnable{
+    private final Queue<String> messageQueue;
+
+    public Consumer() {
+        this.messageQueue = messageQueue;
+    }
+
+    @Override
+    public void run() {
+        while (true){
+            synchronized (messageQueue){
+                while(messageQueue.isEmpty()) {
+                    try{
+                        messageQueue.wait();
+                    } catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+                String message = messageQueue.poll();
+                System.out.println("Consumer consumed: "+message);
+
+                messageQueue.notifyAll();
+            }
+
+            try{
+                Thread.sleep(5000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+
+        }
+    }
 }
--- nangcao3/src/Producer.java
+++ nangcao3/src/Producer.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Producer {
+import java.util.Queue;
+
+public class Producer implements Runnable{
+    private static final int MAX_QUEUE_SIZE = 5;
+    private final Queue<String> messageQueue;
+
+    public Producer(int messageQueue){
+        this.messageQueue = messageQueue;
+    }
+    @Override
+    public void run() {
+        int messageCounter = 1;
+
+        while (true){
+            synchronized (messageQueue){
+                while (messageQueue.size() >= MAX_QUEUE_SIZE){
+                    try{
+                        messageQueue.wait();
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+
+                String message = "Message" + messageCounter;
+                messageQueue.add(message);
+                System.out.println("Producer producer: "+ message);
+                messageCounter++;
+
+                messageQueue.notifyAll();
+            }
+            try{
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
